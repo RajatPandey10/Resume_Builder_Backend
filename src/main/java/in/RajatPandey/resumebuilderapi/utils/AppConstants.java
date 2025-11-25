0ca17cb9fa11921e1c8225a6a96229f4569b377b
RajatPandey10 <pandeyrajat9971@gmail.com>
2025-11-26 00:48:11 +0530

    added all app constants

--- src/main/java/in/RajatPandey/resumebuilderapi/utils/AppConstants.java
+++ src/main/java/in/RajatPandey/resumebuilderapi/utils/AppConstants.java
@@ -5,14 +5,27 @@ public class AppConstants {
     public static final String AUTH_CONTROLLER = "/api/auth";
     public static final String REGISTER = "/register";
     public static final String VERIFY_EMAIL="/verify-email";
-
     public static final String UPLOAD_PROFILE ="/upload-image";
     public static final String LOGIN = "/login";
     public static final String RESEND_VERIFICATION = "/resend-verification";
 
     public static final String PROFILE="/profile";
-    public static final String RESUME = "/api/resumes";
 
+    public static final String RESUME = "/api/resumes";
     public static final String ID = "/{id}";
     public static final String UPLOAD_IMAGES = "/{id}/upload-images";
+
+
+    public static final String PREMIUM = "premium";
+
+    public static final String PAYMENT_CONTROLLER = "/api/payment";
+    public static final String CREATE_ORDER = "/create-order";
+    public static final String VERIFY = "/verify";
+    public static final String HISTORY = "/history";
+    public static final String ORDER_ORDER_ID = "/order/{orderId}";
+
+
+    public static final String EMAIL_CONTROLLER = "/api/email";
+
+    public static final String TEMPLATE_CONTROLLER = "/api/templates";
 }
